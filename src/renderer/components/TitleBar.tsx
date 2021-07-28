32eab0f1356266f55accdf17ed615113a697374a
5onderling <lenny.anders.98@gmail.com>
2021-07-28 22:35:59 +0200

    optimize titlebar

--- src/renderer/components/TitleBar.tsx
+++ src/renderer/components/TitleBar.tsx
@@ -1,50 +1,37 @@
 import { useState } from 'preact/hooks';
+import type { WindowActions } from '../../preload';
 import classes from './TitleBar.module.css';
+
+const Button = ({
+  paths,
+  action,
+}: {
+  paths: string[];
+  action: keyof Pick<WindowActions, 'minimize' | 'maximize' | 'unmaximize' | 'close'>;
+}) => (
+  <button class={classes.windowAction} onClick={window.windowActions[action]}>
+    <svg viewBox='0 0 12 12' class={classes.icon}>
+      {paths.map((path) => (
+        <path d={path} />
+      ))}
+    </svg>
+  </button>
+);
 
 export const TitleBar = () => {
   const [isMaximized, setIsMaximized] = useState(
     window.windowActions.getMaximizedState((isMaximized) => setIsMaximized(isMaximized)),
   );
 
-  const windowAction = (option: 'minimize' | 'maximize' | 'unmaximize' | 'close') => {
-    return () => window.windowActions[option]();
-  };
-
   return (
     <div class={classes.titlebar}>
       <div class={classes.windowActions}>
-        <button class={classes.windowAction} onClick={windowAction('minimize')}>
-          <svg viewBox='0 0 12 12'>
-            <line stroke-width='.9' shape-rendering='crispEdges' x1='1' y1='5.5' x2='11' y2='5.5' />
-          </svg>
-        </button>
-        <button
-          class={classes.windowAction}
-          onClick={windowAction('maximize')}
-          hidden={isMaximized}
-        >
-          <svg viewBox='0 0 12 12' shape-rendering='crispEdges'>
-            <rect stroke-width='.9' x='1.5' y='1.5' width='9' height='9' />
-          </svg>
-        </button>
-        <button
-          class={classes.windowAction}
-          onClick={windowAction('unmaximize')}
-          hidden={!isMaximized}
-        >
-          <svg viewBox='0 0 12 12' stroke-width='.9' shape-rendering='crispEdges'>
-            <rect x='1.5' y='3.5' width='7' height='7' />
-            <polyline points='3.5,3.5 3.5,1.5 10.5,1.5 10.5,8.5 8.5,8.5' />
-          </svg>
-        </button>
-        <button
-          class={`${classes.windowAction} ${classes.windowActionClose}`}
-          onClick={windowAction('close')}
-        >
-          <svg viewBox='0 0 12 12'>
-            <path stroke-width='.9' d='M1,1 l 10,10 M1,11 l 10,-10' />
-          </svg>
-        </button>
+        <Button paths={['M1 5.5h10']} action='minimize' />
+        <Button
+          paths={isMaximized ? ['M1.5 3.5h7v7h-7z', 'M3.5 3.5v-2h7v7h-2'] : ['M1.5 1.5h9v9h-9z']}
+          action={isMaximized ? 'unmaximize' : 'maximize'}
+        />
+        <Button paths={['M1 1l10 10M1 11L11 1']} action='close' />
       </div>
     </div>
   );
