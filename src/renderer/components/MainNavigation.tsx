c84a8346b137f576ccf2c5d1735746ca2bb5216b
5onderling <lenny.anders.98@gmail.com>
2021-07-28 22:13:44 +0200

    MainNavigation -> only use classes in css module

--- src/renderer/components/MainNavigation.tsx
+++ src/renderer/components/MainNavigation.tsx
@@ -9,31 +9,20 @@ import {
 import { Icon } from './icon';
 import classes from './MainNavigation.module.css';
 
+const Link = ({ href, path, text }: { href: string; path: string; text: string }) => (
+  <a href={href} class={classes.link}>
+    <Icon path={path} />
+    {text}
+  </a>
+);
+
 export const MainNavigation = () => (
-  <nav class={classes.mainNavigation}>
-    <a href='#artists'>
-      <Icon path={mdiAccountMusic} />
-      Artists
-    </a>
-    <a href='#albums'>
-      <Icon path={mdiAlbum} />
-      Albums
-    </a>
-    <a href='#songs'>
-      <Icon path={mdiMusicNote} />
-      Songs
-    </a>
-    <a href='#playlists'>
-      <Icon path={mdiPlaylistMusic} />
-      Playlists
-    </a>
-    <a href='#folders'>
-      <Icon path={mdiFolderMusic} />
-      Folders
-    </a>
-    <a href='#settings'>
-      <Icon path={mdiCog} />
-      Settings
-    </a>
+  <nav class={classes.root}>
+    <Link href='#artists' path={mdiAccountMusic} text='Artists' />
+    <Link href='#albums' path={mdiAlbum} text='Albums' />
+    <Link href='#songs' path={mdiMusicNote} text='Songs' />
+    <Link href='#playlists' path={mdiPlaylistMusic} text='Playlists' />
+    <Link href='#folders' path={mdiFolderMusic} text='Folders' />
+    <Link href='#settings' path={mdiCog} text='Settings' />
   </nav>
 );
