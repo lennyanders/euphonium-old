03f07f877cc667b368e87d687e5c60a9642931d3
5onderling <lenny.anders.98@gmail.com>
2021-08-05 22:12:51 +0200

    improve album rendering further

--- src/renderer/components/albums/Album.tsx
+++ src/renderer/components/albums/Album.tsx
@@ -6,21 +6,17 @@ const Cover = ({ album }: { album: AlbumEntity }) => {
   if (!album.previewCoverPath) return <div class={classes.cover} />;
 
   const [showCover, setShowCover] = useState(false);
-  const image = new Image(1, 1);
-  image.src = album.previewCoverPath;
-  image.decode().then(() => setShowCover(true));
 
   return (
     <div class={classes.cover}>
-      {showCover && (
-        <img
-          class={classes.coverImage}
-          src={`file:///${album.previewCoverPath}`}
-          alt={`${album.title} by ${album.artists}`}
-          width='1'
-          height='1'
-        />
-      )}
+      <img
+        class={`${classes.coverImage}${showCover ? ` ${classes.coverImageVisible}` : ''}`}
+        src={`file:///${album.previewCoverPath}`}
+        alt={`${album.title} by ${album.artists}`}
+        width='1'
+        height='1'
+        ref={(image) => image?.decode().then(() => setShowCover(true))}
+      />
     </div>
   );
 };
