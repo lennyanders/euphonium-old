9e13f92f30bb6e23a0a2a320e0a88cbe84be1be3
5onderling <lenny.anders.98@gmail.com>
2021-08-11 21:39:00 +0200

    add option to make progress component vertical

--- src/renderer/components/Progress.tsx
+++ src/renderer/components/Progress.tsx
@@ -2,21 +2,22 @@ import type { Props } from '../utils';
 import classes from './Progress.module.css';
 
 export const Progress = ({
-  value,
-  max,
+  vertical = false,
   ...props
-}: Props<HTMLInputElement, { value: number; max: number }>) => {
+}: Props<
+  HTMLInputElement,
+  { value: number; max: number; vertical?: boolean | string; step?: number }
+>) => {
   return (
-    <div class={classes.progress} style={{ '--progress': `${(value / max) * 100}%` }}>
+    <div
+      class={`${classes.progress}${vertical ? ` ${classes.progressVertical}` : ''}`}
+      style={{
+        '--progress': `${(props.value / props.max) * 100}%`,
+        ...(vertical && { '--verticalSize': typeof vertical === 'string' ? vertical : '5rem' }),
+      }}
+    >
       <div class={classes.progressHandle}></div>
-      <input
-        class={classes.progressInput}
-        type='range'
-        min='0'
-        value={value}
-        max={max}
-        {...props}
-      />
+      <input class={classes.progressInput} type='range' min='0' {...props} />
     </div>
   );
 };
