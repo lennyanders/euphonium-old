518f214d10c27712d760fcdef08c50601b2b8796
5onderling <lenny.anders.98@gmail.com>
2021-08-11 22:22:21 +0200

    use image component for album covers

--- src/renderer/components/albums/Album.tsx
+++ src/renderer/components/albums/Album.tsx
@@ -1,29 +1,17 @@
-import { useState } from 'preact/hooks';
 import { Album as AlbumEntity } from '../../../main/database/entity/album';
+import { Image } from '../Image';
 import classes from './Album.module.css';
-
-const Cover = ({ album }: { album: AlbumEntity }) => {
-  if (!album.previewCoverPath) return <div class={classes.cover} />;
-
-  const [showCover, setShowCover] = useState(false);
-
-  return (
-    <div class={classes.cover}>
-      <img
-        class={`${classes.coverImage}${showCover ? ` ${classes.coverImageVisible}` : ''}`}
-        src={`file:///${album.previewCoverPath}`}
-        alt={`${album.title} by ${album.artists}`}
-        width='1'
-        height='1'
-        ref={(image) => image?.decode().then(() => setShowCover(true))}
-      />
-    </div>
-  );
-};
 
 export const Album = ({ album }: { album: AlbumEntity }) => (
   <li class={classes.album}>
-    <Cover album={album} />
+    <div class={classes.cover}>
+      {album.previewCoverPath && (
+        <Image
+          src={`file:///${album.previewCoverPath}`}
+          alt={`${album.title} by ${album.artists}`}
+        />
+      )}
+    </div>
     <h2 class={classes.albumTitle} title={album.title}>
       {album.title}
     </h2>
